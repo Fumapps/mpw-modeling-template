e6ae9b05bdb448b7c2ed1dfe665f498cce45c740
Mario Fuksa <44242539+fumapps@users.noreply.github.com>
2021-09-10 20:06:19 +0200

    ModelingSetupAcceptanceTest: Fix some template placeholders
    Note: also check if any placeholder still exists in the acceptance test

--- src/test/kotlin/de/unistuttgart/iste/sqa/modeling/setup/ModelingSetupAcceptanceTest.kt
+++ src/test/kotlin/de/unistuttgart/iste/sqa/modeling/setup/ModelingSetupAcceptanceTest.kt
@@ -3,6 +3,7 @@ package de.unistuttgart.iste.sqa.modeling.setup
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.lang.IllegalStateException
 import java.nio.file.Files
 import java.util.concurrent.TimeUnit
 
@@ -24,6 +25,8 @@ internal class ModelingSetupAcceptanceTest {
         val sut = ModelingTemplateProcessor()
         sut.createProjectStructureFromTemplate(configuration)
 
+        assertThatNoPlaceholderIsExistingAnyMore(configuration.targetPath)
+
         val process = ProcessBuilder("mvn", "package", "--file", configuration.targetPath,
             "-Dmaven.repo.local=${configuration.targetPath}/maven-local-repository"
         )
@@ -35,6 +38,12 @@ internal class ModelingSetupAcceptanceTest {
         assertEquals(0, process.exitValue(), process.inputStream.bufferedReader().readText())
 
         cleanup(configuration)
+    }
+
+    private fun assertThatNoPlaceholderIsExistingAnyMore(targetPath: String) {
+        File(targetPath).walkTopDown().filter { it.name.contains("$") }.forEach {
+            throw IllegalStateException("file still contains placeholders: $it")
+        }
     }
 
     private fun gameCommands(vararg names: String) = names.toMutableList()
